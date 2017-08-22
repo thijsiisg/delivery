c1650b60fca9feac147ad6b9df4ff41865f6d940
kerim1 <kerim.meijer@gmail.com>
2017-08-22 13:21:46 +0200

    Update of Spring Boot and enabled scheduling

--- src/main/java/org/socialhistoryservices/Application.java
+++ src/main/java/org/socialhistoryservices/Application.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer{
 
