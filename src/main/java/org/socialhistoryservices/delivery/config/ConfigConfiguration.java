24c932353f4f12f2da3c0fefad1f3fa1738645f4
kerim1 <kerim1@users.noreply.github.com>
2019-08-30 14:05:32 +0200

    accept certificates in development

--- src/main/java/org/socialhistoryservices/delivery/config/ConfigConfiguration.java
+++ src/main/java/org/socialhistoryservices/delivery/config/ConfigConfiguration.java
@@ -7,6 +7,15 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import javax.annotation.PostConstruct;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.text.SimpleDateFormat;
 
 @Configuration
@@ -17,7 +26,7 @@ public class ConfigConfiguration {
     private DeliveryProperties deliveryProperties;
 
     @Bean
-    public SimpleDateFormat dateFormat(){
+    public SimpleDateFormat dateFormat() {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(deliveryProperties.getDateFormat());
         simpleDateFormat.setLenient(false);
         return simpleDateFormat;
@@ -29,5 +38,28 @@ public class ConfigConfiguration {
         ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
         registrationBean.addUrlMappings("/console/*");
         return registrationBean;
+    }
+
+    @PostConstruct
+    @Profile("development")
+    public void trustAll() {
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, new TrustManager[]{new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }}, null);
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        }
+        catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
