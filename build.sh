ab825c6afb91cfcc869424c6cd9c9576b42582ca
Lucien van Wouw <lucien.van.wouw@di.huc.knaw.nl>
2022-07-14 10:00:30 +0200

    xpath voor ander EAD dialiect

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ version=$(git rev-parse master)
 tag=$(git describe --tags)
 name="registry.diginfra.net/${USER}/delivery"
 
-docker_tag=${tag:1}
-docker build --tag="${name}:${docker_tag}" .
-docker push "${name}:${docker_tag}"
+docker build --tag="${name}:${tag}" .
+docker push "${name}:${tag}"
 
 echo "RELEASE=${version} ${tag}"
